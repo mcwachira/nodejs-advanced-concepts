1a8723d8fd518172702a3f9110c3c4163cbe3df6
mcwachira <23430963+mcwachira@users.noreply.github.com>
2023-08-03 20:44:14 +0300

    added clustering fuctionality to improve nodejs perfomance

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
+const cluster = require('cluster')
+//Is the file being executed in master mode
+
+if(cluster.isMaster) {
+//Cause index.js to be executed again but in child mode
+    cluster.fork()
+    // cluster.fork()
+    // cluster.fork()
+    // cluster.fork()
+
+}else{
+
+
+
 const express =  require('express')
 
-const app = express()
+const app = express();
 
+function doWorks(duration){
+    const start = Date.now()
+    while(Date.now() - start  <duration) {}
+}
 app.get('/', (req, res) => {
+
+  doWorks(5000)
     res.send('HI there')
 })
 
+    app.get('/fast', (req, res) => {
+
+        res.send('This was fast!')
+    })
+
 app.listen(3000)
+
+}
